7d3bb1cca81a5bdadc7c61887d3fac6a83d54a2e
srnosoriano12 <61635161+jlsr1999@users.noreply.github.com>
2020-10-15 15:29:05 -0500

    Clase de operaciones

--- src/main/java/Modelo/Operaciones.java
+++ src/main/java/Modelo/Operaciones.java
@@ -7,10 +7,64 @@ package Modelo;
 
 /**
  * Clase encargada de realizar las operaciones
- * @author Sergio Sabogal
+ * @author Jose Luis Soriano Roa
  * @since 15/10/2020
  * @version  1.0.0
  */
 public class Operaciones {
+
+    private double numeroUno;
+    private double numeroDos;
+    
+    /**
+     * Constuctor de la clase
+     * @param numeroUno numero uno de la operacion
+     * @param numeroDos numero dos de la operacion
+     */
+    
+    public Operaciones(double numeroUno, double numeroDos) {
+        this.numeroUno = numeroUno;
+        this.numeroDos = numeroDos;
+    }
+    
+    /**+
+     * Retonra el numero uno
+     * @return numerouno
+     */
+    
+    public double getNumeroUno() {
+        return numeroUno;
+    }
+    
+    /**
+     * recibe el numero uno de la operacion
+     * @param numeroUno numero uno de la operacion
+     */
+    public void setNumeroUno(double numeroUno) {
+        this.numeroUno = numeroUno;
+    }
+    /**
+     * retorna el numero dos de la operacion
+     * @return numero dos de la operacion
+     */
+    public double getNumeroDos() {
+        return numeroDos;
+    }
+    /**
+     * recibe el numero dos de la operacion
+     * @param numeroDos 
+     */
+    public void setNumeroDos(double numeroDos) {
+        this.numeroDos = numeroDos;
+    }
+    public double sumar(){
+        return this.numeroDos + this.numeroUno;
+    }
+    public double restar(){
+        return this.numeroDos - this.numeroUno;
+    }
+    public double multiplicar(){
+        return this.numeroDos * this.numeroUno;
+    }
     
 }
