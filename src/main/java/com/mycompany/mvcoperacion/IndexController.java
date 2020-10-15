d96cc4ac51e3237fbef6e042fba1466972bdf35d
kote00789 <kote_alejo92@gmail.com>
2020-10-15 16:38:31 -0500

    Implementacion de metodos
    Metodos de suma , resta y multiplicacion

--- src/main/java/com/mycompany/mvcoperacion/IndexController.java
+++ src/main/java/com/mycompany/mvcoperacion/IndexController.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.mycompany.mvcoperacion;
-
+import Modelo.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -17,6 +17,9 @@ import javafx.scene.control.TextField;
  * FXML Controller class
  *
  * @author Jose Luis
+ * @author Sergio Sabogal
+ * @since 15/10/2020
+ * @version 1.0.0
  */
 public class IndexController implements Initializable {
 
@@ -35,19 +38,69 @@ public class IndexController implements Initializable {
         // TODO
     }    
 
+    
+    /**
+     * Este metodo se encarga de hacer las operaciones de Suma
+     * @param event 
+     */
     @FXML
     private void sumar(ActionEvent event) {
-        System.out.println("suma");
+        try{
+            validar();
+            double numero1 =    Double.parseDouble(num1.textProperty().get());
+            double numero2 =    Double.parseDouble(num2.textProperty().get());
+            resultado.setText( new Operaciones(numero1, numero2).sumar()+ " ");
+            
+        }catch(ArithmeticException ex){
+            resultado.setText("Error en datos");    
+        }catch(Exception exi){
+            resultado.setText("Error en datos");    
+        } 
     }
-
+     /**
+     * Este metodo se encarga de hacer las operaciones de Resta
+     * @param event 
+     */
     @FXML
     private void Restar(ActionEvent event) {
-        System.out.println("resta");
+        try{
+            validar();
+            double numero1 =    Double.parseDouble(num1.textProperty().get());
+            double numero2 =    Double.parseDouble(num2.textProperty().get());
+            resultado.setText( new Operaciones(numero1, numero2).restar()+ " ");
+            
+        }catch(ArithmeticException ex){
+            resultado.setText("Error en datos");    
+        }catch(Exception exi){
+            resultado.setText("Error en datos");    
+        }
     }
 
+    /**
+     * Este metodo se encarga de hacer las operaciones de Multiplicar
+     * @param event 
+     */
     @FXML
     private void Multiplicar(ActionEvent event) {
-        System.out.println("multiplica");
+        try{
+            validar();
+            double numero1 =    Double.parseDouble(num1.textProperty().get());
+            double numero2 =    Double.parseDouble(num2.textProperty().get());
+            resultado.setText( new Operaciones(numero1, numero2).multiplicar()+ " ");
+        }catch(ArithmeticException ex){
+            resultado.setText("Error en datos");    
+        }catch(Exception exi){
+            resultado.setText("Error en datos");    
+        }
+    }
+    /**
+     * Este metodo se encarga de hacer la validación de los campos para que no esten vacios
+     * @param event 
+     */
+    public void validar(){
+        if(num1.textProperty().get().isEmpty() || num2.textProperty().get().isEmpty()){
+            resultado.setText("ingresa los datos");    
+        }
     }
     
 }
