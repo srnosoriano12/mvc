21ad13d2ebaab464ccd1304828c5e56877237c97
jlsr1999 <61635161+jlsr1999@users.noreply.github.com>
2020-10-15 15:15:19 -0500

    creacion del proyecto

---
+++ src/main/java/Modelo/Operaciones.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ * Clase encargada de realizar las operaciones
+ * @author Sergio Sabogal
+ * @since 15/10/2020
+ * @version  1.0.0
+ */
+public class Operaciones {
+    
+}
